e314e9d19e5244e4b684114e7d619d7158a36d80
trinity86 <55467751+trinity86@users.noreply.github.com>
2021-06-22 12:56:39 +0300

    Add files via upload

---
+++ src/test/java/Test.java
@@ -0,0 +1,52 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Test {
+  @org.junit.Test
+    public void test() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.opencart.com/");
+      WebDriverWait wait = new WebDriverWait(driver, 5);
+        driver.manage().window().maximize();
+
+        // Atidarome visas prekes Laptops and Notebooks
+      driver.findElement(By.linkText("Laptops & Notebooks")).click();
+      driver.findElement(By.linkText("Show All Laptops & Notebooks")).click();
+
+      //paspaudziame ant kiekvienos prekes ir patikriname ar available
+      driver.findElement(By.linkText("HP LP3065")).click();
+      driver.findElement(By.xpath("//li[text()='Availability: In Stock']"));
+      WebElement element = wait.until(
+              ExpectedConditions.presenceOfElementLocated(By.xpath("//li[text()='Availability: In Stock']"))
+      );
+      Assert.assertTrue(element.getText().contains("Availability: In Stock"));
+      driver.navigate().back();
+      driver.findElement(By.linkText("MacBook")).click();
+      Assert.assertTrue(element.getText().contains("Availability: In Stock"));
+          driver.navigate().back();
+      driver.findElement(By.linkText("MacBook Air")).click();
+      Assert.assertTrue(element.getText().contains("Availability: In Stock"));
+      driver.navigate().back();
+           driver.findElement(By.linkText("MacBook Pro")).click();
+      Assert.assertTrue(element.getText().contains("Availability: In Stock"));
+      driver.navigate().back();
+      driver.findElement(By.linkText("Sony VAIO")).click();
+      Assert.assertTrue(element.getText().contains("Availability: In Stock"));
+
+      //Atidarome visas prekes Tablets
+      driver.findElement(By.linkText("Tablets")).click();
+      //paspaudziame ant kiekvienos prekes ir patikriname ar available
+      driver.findElement(By.xpath("//div[@class='caption']//a[1]")).click();
+      //Atidarome visas prekes Phones and PDAs
+      driver.findElement(By.linkText("Phones & PDAs")).click();
+
+    }
+}
